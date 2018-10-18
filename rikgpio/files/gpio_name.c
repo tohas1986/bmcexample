1890b9b5cd94375bb3723b42b552b83c83ea016f
tohas1986 <kapitanov.anton@mail.ru>
2018-10-18 13:46:03 +0300

    added rikgpio

---
+++ rikgpio/files/gpio_name.c
@@ -0,0 +1,29 @@
+#include "gpio.h"
+
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <poll.h>
+#include <string.h>
+#include <ctype.h>
+#include <pthread.h>
+
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/errno.h>
+
+//#include <openbmc/log.h>
+
+int
+gpio_num(char *str)
+{
+  return -1;
+}
+
+void main(void)
+{
+  printf("HEy...WellDONE!! :)...\n");
+  return NULL;
+}
+
